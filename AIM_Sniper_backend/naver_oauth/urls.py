c64087d3030447161712d1cf3465b414b2441dc8
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-09-20 10:38:03 +0900

    feat: [김용현] naver uri를 통해 회원 정보 토큰을 가져옵니다 [AIMSBD-18]

--- AIM_Sniper_backend/naver_oauth/urls.py
+++ AIM_Sniper_backend/naver_oauth/urls.py
@@ -10,3 +10,5 @@ router.register(r'naver_oauth', NaverOauthView, basename='naver_oauth')
 urlpatterns = [
     path('', include(router.urls)),
     path('naver', NaverOauthView.as_view({'get': 'naverOauthURI'}), name='get-naver-oauth-uri'),
+    path('naver/access-token', NaverOauthView.as_view({'post': 'naverAccessTokenURI'}),
+                                name='get-naver-access-token-uri'),
