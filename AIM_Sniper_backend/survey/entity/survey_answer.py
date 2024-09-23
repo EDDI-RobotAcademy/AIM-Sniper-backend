939ad7139bb54982c95fd828d53b626f787bdfde
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-09-23 12:50:34 +0900

    add: [최민지] answer table에 사용자 정보 추가 [AIMSBD-24]

--- AIM_Sniper_backend/survey/entity/survey_answer.py
+++ AIM_Sniper_backend/survey/entity/survey_answer.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from account.entity.account import Account
 from survey.entity.survey_question import SurveyQuestion
 from survey.entity.survey_selection import SurveySelection
 
@@ -7,6 +8,7 @@ class SurveyAnswer(models.Model):
     id = models.AutoField(primary_key=True)
     survey_selection_id = models.ForeignKey(SurveySelection, on_delete=models.CASCADE, db_column='survey_selection_id')
     survey_question_id = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE, db_column='survey_question_id')
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, db_column='account_id')
 
     class Meta:
         db_table = 'survey_answer'
