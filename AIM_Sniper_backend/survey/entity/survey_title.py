96b903e8e6366c41a882b3ea8c307e2e00b08400
y0ng98 <dldydgnl2080@gmail.com>
2024-09-24 16:38:28 +0900

    fix: survey 관련 항목 중복 허용

--- AIM_Sniper_backend/survey/entity/survey_title.py
+++ AIM_Sniper_backend/survey/entity/survey_title.py
@@ -5,7 +5,7 @@ from survey.entity.survey import Survey
 
 class SurveyTitle(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=64, unique=True)
+    title = models.CharField(max_length=64, unique=False)
     survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
 
     class Meta:
