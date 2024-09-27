baa7173381ec116ba8a0e0feb3ecc836b1aee229
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-09-27 14:10:24 +0900

    fix: [] 설문 응답 저장 시 survey id가 초기화되어 들어가던 문제 수정 [AIMSBD-24]

--- AIM_Sniper_backend/survey/repository/survey_selection_repository.py
+++ AIM_Sniper_backend/survey/repository/survey_selection_repository.py
@@ -11,5 +11,9 @@ class SurveySelectionRepository(ABC):
         pass
 
     @abstractmethod
-    def findSelection(self, selectionId):
+    def findSelection(self, selection):
+        pass
+
+    @abstractmethod
+    def findSelectionBySelectionName(self, selectionName):
         pass
