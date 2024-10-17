f9991e3de1e53872585035b0cdb1e8b3e8a7db02
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-10-17 17:20:13 +0900

    feat: [김용현] 키워드 분석 후 분류를 위해서 keyword column을 생성합니다.[AIMSDB-54]

--- AIM_Sniper_backend/company_report/serializers.py
+++ AIM_Sniper_backend/company_report/serializers.py
@@ -6,5 +6,5 @@ from company_report.entity.models import CompanyReport
 class CompanyReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyReport
-        fields = ['companyReportId', 'companyReportName', 'companyReportPrice','companyReportTitleImage','companyReportCategory', 'content', 'regDate', 'updDate']
+        fields = ['companyReportId', 'companyReportName', 'companyReportPrice','companyReportTitleImage','companyReportCategory','keyword', 'content', 'regDate', 'updDate']
         read_only_fields = ['regDate', 'updDate']
